dccd32d2ee17f286aa397a07a0b4a18befc4d24f
Garaje de Ideas <82031166+GarajedeIdeas@users.noreply.github.com>
2023-12-29 10:35:48 +0100

    Add file

--- src/providers/RegFormProvider.jsx
+++ src/providers/RegFormProvider.jsx
@@ -1,1 +1,35 @@
+import { createContext, useContext, useReducer } from "react";
 
+const RegFormContext = createContext();
+export const useRegFormContext = () => {
+    return useContext(RegFormContext);
+}
+
+const reducer = (state, action) => {
+    // { type, data }
+    switch (action.type) {
+        case 'SET_COMMON_DATA': {
+            return { ...state, common: { ...action.data } };
+        }
+        case 'SET_ADDRESS_DATA': {
+            return { ...state, address: { ...action.data } };
+        }
+        case 'SET_DESCRIPTION': {
+            return { ...state, description: action.data };
+        }
+        case 'CHANGE_PERCENT': {
+            return { ...state, percent: action.data };
+        }
+    }
+    return state;
+}
+
+const RegFormProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, { percent: 0 });
+
+    return <RegFormContext.Provider value={[state, dispatch]}>
+        {children}
+    </RegFormContext.Provider>
+}
+
+export default RegFormProvider;
